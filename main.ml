d44302a8b74e16ffd786d5b9c57b48d912930205
Aaron L. Zeng <a2z@mit.edu>
2016-09-02 03:46:53 -0400

    Complete first working version

--- main.ml
+++ main.ml
@@ -1,31 +1,32 @@
 open! Core.Std
 open! Async.Std
 
-let pool_data id =
-  let url =
-    "http://danbooru.donmai.us/pools/" ^ Int.to_string id ^ ".json"
-    |> Uri.of_string
-  in
+let get url =
   Cohttp_async.Client.get url
   >>| fun (response, body) ->
   match response.status with
   | `OK -> Ok body
   | _   -> Or_error.error_string (Cohttp.Code.string_of_status response.status)
+
+let pool_data id =
+  "http://danbooru.donmai.us/pools/" ^ Int.to_string id ^ ".json"
+  |> Uri.of_string
+  |> get
+;;
+
+let string_of_body =
+  function
+  | `Empty  -> Deferred.return ""
+  | `Pipe r ->
+    let%map strings = Pipe.read_all r >>| Queue.to_list in
+    String.concat strings
+  | `String s -> Deferred.return s
+  | `Strings s -> Deferred.return (String.concat s)
 ;;
 
 let json_of_body body =
-  let body =
-    match body with
-    | `Empty  -> Deferred.Or_error.error_string "empty body"
-    | `Pipe r ->
-      let%map strings = Pipe.read_all r >>| Queue.to_list in
-      Ok (String.concat strings)
-    | `String s -> Deferred.Or_error.return s
-    | `Strings s -> Deferred.Or_error.return (String.concat s)
-  in
-  let open Deferred.Or_error.Let_syntax in
-  let%map body = body in
-  Yojson.Basic.from_string body
+  let%map body = string_of_body body in
+  Or_error.try_with (fun () -> Yojson.Basic.from_string body)
 ;;
 
 let read_posts json =
@@ -46,8 +47,49 @@ let pool_posts id =
   Or_error.(json >>= read_posts)
 ;;
 
+let post_data id =
+  "http://danbooru.donmai.us/posts/" ^ Int.to_string id ^ ".json"
+  |> Uri.of_string
+  |> get
+;;
+
+let read_file_url json =
+  match json with
+  | `Assoc mappings ->
+    List.Assoc.find mappings "file_url"
+    |> begin function
+    | Some (`String file_url) -> Ok file_url
+    | Some _ -> Or_error.error_string "[file_url] value is not a string"
+    | None   -> Or_error.error_string "no [file_url] field"
+    end
+  | _ -> Or_error.error_string "not a JSON object"
+;;
+
+let basename_of_file_url url =
+  String.rsplit2 url ~on:'/'
+  |> function
+  | Some (_, basename) -> Ok basename
+  | None -> Or_error.error_string "no / in file url"
+
 let download_post id =
-  failwith "Unimplemented"
+  let%bind post_json = id |> post_data >>=? json_of_body in
+  match post_json with
+  | Ok json ->
+    begin match read_file_url json with
+    | Ok file_url ->
+      let filename = basename_of_file_url file_url in
+      let url = "http://danbooru.donmai.us" ^ file_url |> Uri.of_string in
+      let%bind contents =
+        let string_of_body body = string_of_body body >>| Or_error.return in
+        url |> get >>=? string_of_body
+      in
+      begin match Or_error.both filename contents with
+      | Ok (name, contents) -> Writer.with_file name ~f:(fun w -> Writer.write w contents; Writer.close w)
+      | Error e -> eprintf !"%{sexp: Error.t}\n" e; Deferred.unit
+      end
+    | Error e -> eprintf !"%{sexp: Error.t}\n" e; Deferred.unit
+    end
+  | Error e -> eprintf !"%{sexp: Error.t}\n" e; Deferred.unit
 ;;
 
 let command =
@@ -55,8 +97,11 @@ let command =
     let open Command.Let_syntax in
     let%map_open pool_id = anon ("id" %: int) in
     fun () ->
-      pool_posts pool_id
-      >>|? List.iter ~f:download_post
+      let open Deferred.Or_error.Let_syntax in
+      let%bind posts = pool_posts pool_id in
+      Pipe.of_list posts
+      |> Pipe.iter ~f:download_post
+      >>| Or_error.return
   end
 ;;
 
