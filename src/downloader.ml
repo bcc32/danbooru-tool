641a81ef46af3039653a181c5dde7f4c0bc1c4e0
Aaron L. Zeng <a2z@mit.edu>
2017-04-17 05:23:01 -0400

    Reorganize

--- src/downloader.ml
+++ src/downloader.ml
@@ -15,17 +15,14 @@ let download id ~basename =
   Deferred.Or_error.(get_post () >>= save_post)
 ;;
 
-let pad digits =
-  sprintf "%0*d" digits
-;;
-
 let download_posts ids ~naming_scheme =
   let deferreds =
     match naming_scheme with
     | `Md5 -> List.map ids ~f:(download ~basename:`Md5)
     | `Sequential ->
       let digits = List.length ids |> Int.to_string |> String.length in
-      List.mapi ids ~f:(fun i -> download ~basename:(`Basename (pad digits i)))
+      let pad = sprintf "%0*d" digits in
+      List.mapi ids ~f:(fun i -> download ~basename:(`Basename (pad i)))
   in
   Deferred.Or_error.all_ignore deferreds
 ;;
