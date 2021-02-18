f46ccd1e25ee31638bb0aaae27883926851db742
Aaron L. Zeng <me@bcc32.com>
2021-02-18 01:49:06 -0500

    Format and document shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [ m4 libffi openssl pkgconfig ];
+
+with pkgs;
+mkShell {
+  buildInputs = [
+    # For opam
+    m4
+    opam
+    pkgconfig
+    # For this project
+    openssl
+  ];
 }
