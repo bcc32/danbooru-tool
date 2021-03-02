2bf353b67929bda61323942422c80316284b1bd8
Aaron L. Zeng <me@bcc32.com>
2021-03-01 22:30:43 -0500

    Separate test dependencies into checkInputs

--- default.nix
+++ default.nix
@@ -1,21 +1,13 @@
 { lib, nix-gitignore, buildDunePackage, async, async_ssl, cmdliner, core, cohttp
 , cohttp-async, expect_test_helpers_async, expect_test_helpers_core, yojson }:
 
-buildDunePackage {
+buildDunePackage rec {
   pname = "danbooru-tool";
   version = "0.4.1";
   useDune2 = true;
   src = nix-gitignore.gitignoreFilterSource lib.cleanSourceFilter [ ] ./.;
-  buildInputs = [
-    async
-    async_ssl
-    cmdliner
-    core
-    cohttp
-    cohttp-async
-    expect_test_helpers_async
-    expect_test_helpers_core
-    yojson
-  ];
+  checkInputs = [ expect_test_helpers_async expect_test_helpers_core ];
+  buildInputs = [ async async_ssl cmdliner core cohttp cohttp-async yojson ];
+  passthru.checkInputs = checkInputs;
   meta = { homepage = "https://github.com/bcc32/danbooru-tool"; };
 }
