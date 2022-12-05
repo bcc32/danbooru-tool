9b12b1d726dfdbc9fdba557d1d3cebd4a2c08480
Aaron L. Zeng <me@bcc32.com>
2022-12-05 01:47:08 -0500

    Update tests

--- test/test_banned_post.ml
+++ test/test_banned_post.ml
@@ -5,10 +5,11 @@ open! Import
 let%expect_test "download banned post" =
   test (fun (module Testbooru) ->
     let open Testbooru in
-    let%bind post = Post.get 36 >>| ok_exn in
+    let%bind post = Post.get 3496 >>| ok_exn in
     let%bind () =
       require_does_raise_async [%here] (fun () ->
         Post.download post ~basename:`Md5 >>| ok_exn)
     in
-    [%expect {| ("Post missing download information" (post_id 36)) |}])
+    [%expect {| ("Post missing download information" (post_id 3496)) |}];
+    return ())
 ;;
--- test/test_post.ml
+++ test/test_post.ml
@@ -7,7 +7,8 @@ let%expect_test "download single post" =
     let open Testbooru in
     let%bind post = Post.get 1 >>| ok_exn in
     let%bind () = Post.download post ~basename:`Md5 >>| ok_exn in
-    let%bind file_exists = Sys.file_exists_exn "f49cdde9bcd7edb54e597e8c21777dbb.png" in
+    let%bind file_exists = Sys.file_exists_exn "ba4420f5f545e99479c1cb12cfea65b2.png" in
     require [%here] file_exists;
-    [%expect {| |}])
+    [%expect {| |}];
+    return ())
 ;;
